cb8c1ea120c4fe0e55b7beef7c34df444b71a40b
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-11-25 15:54:45 +0530

    finished

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 // Container With Most Water
 // Given n non-negative integers a1, a2, ..., an , where each represents a point
 // at coordinate (i, ai). n vertical lines are drawn such that the two endpoints
@@ -9,6 +11,19 @@
 
 // The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
 // In this case, the max area of water (blue section) the container can contain is 49.
+maxWater(List<int> containers){
+  List<int> area=[];
+  for(int i=0;i<containers.length-1;i++){
+    for(int j=i+1;j<containers.length;j++){
+      int temp=(j-i)*min(containers[i],containers[j]);
+      area.add(temp);
+    }
+  }
+  // print(Area);
+  return (area.reduce((curr, next) => curr > next? curr: next));
+}
 
 main() {
+  print(maxWater([1,4,3,5]));
+  print(maxWater([4,8,6,7,6,5]));
 }
--- test/Day_37_Morning_Challenge_test.dart
+++ test/Day_37_Morning_Challenge_test.dart
@@ -1,7 +1,17 @@
+import 'dart:convert';
+
 import 'package:test/test.dart';
 import '../bin/main.dart';
 
 void main() {
-  test('Test Case 1', () {
+
+  test('maximum water', () {
+    expect(maxWater([1,4,3,5]),8);
+  });
+  test('maximum water', () {
+    expect(maxWater([4,8,6,7,6,5]),20);
+  });
+  test('maximum water', () {
+    expect(maxWater([1,8,6,2,5,4,8,3,7]),49);
   });
 }
